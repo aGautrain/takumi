7afd88e3d060273271f5bbdf8aab80e1dea2927f
Antoine <antoine.gautrain@mediego.com>
2022-04-09 01:00:40 +0200

    fix(nfts): api replacing reactor urls

--- src/app/services/explorers/nfts-api.service.ts
+++ src/app/services/explorers/nfts-api.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
+import {map} from "rxjs/operators";
 
 export interface NFTCollectionResult {
   ownedNfts: OwnedNFT[];
@@ -29,6 +30,8 @@ export interface OwnedNFT {
   };
   media: Array<{ raw: string; gateway: string; }>;
   timeLastUpdated: string;
+
+  error?: unknown;
 }
 
 @Injectable({
@@ -44,6 +47,23 @@ export class NftsApiService {
   getNFTS(address: string): Promise<NFTCollectionResult> {
     return this.http.get<NFTCollectionResult>(
       `${this.API_ENDPOINT}/v2/demo/getNFTs?owner=${address}`
+    ).pipe(
+      map((response) => {
+
+        // excluding incorrect NFTs
+        response.ownedNfts = response.ownedNfts.filter(nft => !nft?.error && nft?.id?.tokenMetadata?.tokenType === 'ERC721');
+
+        response.ownedNfts = response.ownedNfts.map(nft => {
+          // special catch for Reactor NFTs thumbnail urls
+          if (nft?.contract?.address === '0x6f9eb87f5a5638a3424c68ffae824608671f4ea6') {
+            nft.media[0].gateway = nft.media[0].gateway.replace('thumbnails', 'thumbnails_cars');
+          }
+
+          return nft;
+        });
+
+        return response;
+      })
     ).toPromise() as Promise<NFTCollectionResult>;
   }
 }
